5731a7ee4c34bd6b7bcffd036ac29ed1af5f3183
Nikita Karamov <nick@karamoff.dev>
2020-12-14 09:08:06 +0100

    Initial code

---
+++ api/ip.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var ipStr string
+	ipStr = r.Header.Get("X-Real-Ip")
+
+	if ipStr == "" {
+		ipArr := strings.Split(r.Header.Get("X-Forwarded-For"))
+		if len(ipArr) > 0 {
+			ipStr = ipArr[0]
+		}
+	}
+
+	if ipStr == "" {
+		ipStr = r.RemoteAddr
+	}
+
+	if ipStr == "" {
+		ipStr = "0.0.0.0"
+	}
+
+	fmt.Fprintf(w, ipStr)
+}
