51aeca7014972178d8f14b39eced9c908c93216c
Nikita Karamov <me@kytta.dev>
2024-07-28 16:46:14 +0200

    Use a different function API

--- api/ip.js
+++ api/ip.js
@@ -18,20 +18,20 @@
 
 import { getClientIp } from "@supercharge/request-ip";
 
+
 /**
  * Handles request and returns user's IP address in response
  *
- * @param {import("@vercel/node").VercelRequest} req
- * @param {import("@vercel/node").VercelResponse} res
+ * @param {Request} req
+ * @returns {Response}
  */
-export default (req, res) => {
-	res.setHeader("Content-Type", "text/plain");
+export function GET(request) {
+	const ip = getClientIp(request);
 
-	if (req.method !== 'GET') {
-		res.status(405).send("Method not allowed.");
-		return;
-	}
-
-	const ip = getClientIp(req);
-	res.status(ip ? 200 : 404).send(ip ? ip : "0.0.0.0");
+	return new Response(ip ?? "0.0.0.0", {
+		headers: {
+			"Content-Type": "text/plain"
+		},
+		status: ip ? 200 : 404
+	})
 }
